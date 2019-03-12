1e482e4c2137f37bcf5b2e1239893433773d542e
Andhika Tirtawisata <andhikatirtawisata@Andhikas-MacBook-Air.local>
2019-03-12 15:31:06 +0700

    UI with connections

--- Apple Pie 2/ViewController.swift
+++ Apple Pie 2/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
